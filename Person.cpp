8192de655e4a018a99f4f4f26a476d004e7fa670
Repnikov Pavel <prepnik@mail.ru>
2022-08-29 23:21:24 +0300

    playing animation asset from cpp works well

--- Person.cpp
+++ Person.cpp
@@ -1,40 +1,80 @@
 
 #include "Person.h"
 
+#include "NavigationSystemTypes.h"
+
 APerson::APerson()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	SkeletalMeshComponentPtr = CreateDefaultSubobject<USkeletalMeshComponent>("SkeletalMeshComp"); //analog of the new operator
+	SkeletalMeshComponentPtr = CreateDefaultSubobject<USkeletalMeshComponent>("SkeletalMeshComponent");
 	SkeletalMeshComponentPtr->SetupAttachment(GetRootComponent());
-	LoadAndSetSkeletalMesh();
 	
+	SpringArmComponentPtr=CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+	SpringArmComponentPtr->SetupAttachment(GetMyMesh());
+	
+	CameraComponentPtr = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+	CameraComponentPtr->SetupAttachment(GetMySpringArm());
+	
+	LoadAndSetSkeletalMesh(PATH_SKELETAL_MESH_ARMS);
+	AssignSpringArmVaribles(FVector(0.0f, 0.0f, 50.0f), FRotator(-60.0f, 90.0f, 0.0f),400.0f,3.0f);
+	LoadAnimationAsset(PATH_ANIMATION_ASSET_FPS_IDLE);
+	
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 // Ctor functions
-void APerson::LoadAndSetSkeletalMesh()
+void APerson::LoadAndSetSkeletalMesh(const char* PathToSkeletalMesh)
 {
-	const char* bytes= PATH_SKELETAL_MESH_ARMS;
-	Loader<USkeletalMesh> Loader(FString(strlen(bytes),bytes));
-	SkeletalMeshComponentPtr->SetSkeletalMesh(Loader.GetDataPtr());
+	Loader<USkeletalMesh> Loader(FString(strlen(PathToSkeletalMesh),PathToSkeletalMesh));
+	GetMyMesh()->SetSkeletalMesh(Loader.GetDataPtr());
+}
+void APerson::AssignSpringArmVaribles(const FVector& RelLocation,const FRotator& RelRotation,const double ArmLength, const double CameraLagSpeed)
+{
+	SpringArmComponentPtr->SetRelativeLocationAndRotation(RelLocation,RelRotation);
+	SpringArmComponentPtr->TargetArmLength = ArmLength;
+	if (CameraLagSpeed != 0.0)
+	{
+		SpringArmComponentPtr->bEnableCameraLag = true;
+		SpringArmComponentPtr->CameraLagSpeed= CameraLagSpeed;
+	}
+	
+}
+void APerson::LoadAnimationAsset(const char* PathToAnimationAsset)
+{
+	Loader<UAnimationAsset> Loader(FString(strlen(PathToAnimationAsset),PathToAnimationAsset));
+	AnimationAssetPtr = Loader.GetDataPtr();
 }
 //
+
 
 void APerson::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 void APerson::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
 }
 
 void APerson::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// IDLE functionality
+	PlayerInputComponent->BindAction("IDLE_full_stamina",IE_Pressed,this,&APerson::PlayFPSIdle);
+
+}
+
+//animations callback functions:
+void APerson::PlayFPSIdle()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow,"playing FPS idle");
+	}
+	GetMyMesh()->PlayAnimation(AnimationAssetPtr,true);
 }
 
 const char* BoolToCStr(bool bool_value)
@@ -42,3 +82,13 @@ const char* BoolToCStr(bool bool_value)
 	return (bool_value?"true":"false");
 }
 
+
+//getters:
+USkeletalMeshComponent* APerson::GetMyMesh() const
+{
+	return SkeletalMeshComponentPtr;
+}
+USpringArmComponent* APerson::GetMySpringArm() const
+{
+	return SpringArmComponentPtr;
+}
--- Person.h
+++ Person.h
@@ -4,13 +4,62 @@
 #include "GameFramework/Character.h"
 #include <string>
 #include "LoggingSystem.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "Person.generated.h"
 
 #define CHECK_SUCCESS_OF_LOAD_DATA
 
-#define PATH_SKELETAL_MESH_ARMS "/Game/FPS_Assets/Characters/Arms/Mesh/Mesh_Arms"
+#define PATH_SKELETAL_MESH_ARMS "/Game/FPS_Content/Assets/Characters/Arms/Mesh/Mannequin/Mesh/SK_Mannequin_Arms"
+#define PATH_ANIMATION_ASSET_FPS_IDLE "/Game/FPS_Content/Assets/Characters/Arms/Arms_Animations/Arms_idle"
+
 
 const char* BoolToCStr(bool bool_value);
+
+
+UCLASS()
+class FPS_API APerson : public ACharacter
+{
+	GENERATED_BODY()
+private:
+	//components:
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* SkeletalMeshComponentPtr;
+	
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComponentPtr; 
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComponentPtr;
+
+	//assets:
+	UPROPERTY(VisibleAnywhere)
+	UAnimationAsset* AnimationAssetPtr; 
+
+	//getters:
+protected:
+	USkeletalMeshComponent* GetMyMesh() const;
+	USpringArmComponent* GetMySpringArm() const;
+public:
+	APerson();
+	//ctor functions:
+private:
+	void LoadAndSetSkeletalMesh(const char* PathToSkeletalMesh);
+	void AssignSpringArmVaribles(const FVector& RelLocation,const FRotator& RelRotation,const double ArmLength, const double CameraLagSpeed);
+	void LoadAnimationAsset(const char* PathToAnimationAsset);
+	//begin play fucntions:
+private:
+	//animation callback functions:
+	void PlayFPSIdle();
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
 
 template<typename TypeToLoad>
 class Loader
@@ -41,25 +90,3 @@ public:
 private:
 	TypeToLoad* _DataPtr;
 };
-
-UCLASS()
-class FPS_API APerson : public ACharacter
-{
-	GENERATED_BODY()
-private:
-	UPROPERTY(VisibleAnywhere)
-	USkeletalMeshComponent* SkeletalMeshComponentPtr;
-public:
-	APerson();
-private:
-	void LoadAndSetSkeletalMesh();
-protected:
-	virtual void BeginPlay() override;
-
-public:	
-	virtual void Tick(float DeltaTime) override;
-	
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-};
-
