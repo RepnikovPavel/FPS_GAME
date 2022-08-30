29d0d11dff6a8e801bb1b716dbaea5c9bf92ad40
Repnikov Pavel <prepnik@mail.ru>
2022-08-30 14:44:25 +0300

    transferring content to a class Pawn

--- Person.cpp
+++ Person.cpp
@@ -1,10 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #include "Person.h"
+#include <string>
+#include "LoggingSystem.h"
 
-#include "NavigationSystemTypes.h"
+#define ENABLE_LOGGING_ON_SCREEN_ACTION_MAPPINGS
+#define CHECK_SUCCESS_OF_LOAD_DATA
+
+#define M_ACM_Jump "Jump"
+#define M_AXM_MoveForward "MoveForward"
+#define M_AXM_MoveRight "MoveRight"
+#define M_AXM_TurnCameraX "TurnCameraX"
+#define M_AXM_TurnCameraY "TurnCameraY"
+#define M_AXM_ChangeSpringArmLength "ChangeSpringArmLength"
+
+#define PATH_SKELETAL_MESH_ARMS "/Game/FPS_Content/Assets/Characters/Arms/Mesh/Mannequin/Mesh/SK_Mannequin_Arms"
+#define PATH_ANIMATION_ASSET_FPS_IDLE "/Game/FPS_Content/Assets/Characters/Arms/Arms_Animations/Arms_idle"
 
 APerson::APerson()
 {
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	SkeletalMeshComponentPtr = CreateDefaultSubobject<USkeletalMeshComponent>("SkeletalMeshComponent");
@@ -17,11 +33,45 @@ APerson::APerson()
 	CameraComponentPtr->SetupAttachment(GetMySpringArm());
 	
 	LoadAndSetSkeletalMesh(PATH_SKELETAL_MESH_ARMS);
-	AssignSpringArmVaribles(FVector(0.0f, 0.0f, 50.0f), FRotator(-60.0f, 90.0f, 0.0f),400.0f,3.0f);
+	AssignSpringArmVaribles(FVector(0.0f, 0.0f, 50.0f), FRotator(-60.0f, 0.0f, 0.0f),400.0f,3.0f);
 	LoadAnimationAsset(PATH_ANIMATION_ASSET_FPS_IDLE);
 	
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
+
+// helper classes:
+const char* BoolToCStr(bool bool_value);
+
+template<typename TypeToLoad>
+class Loader
+{
+public:
+	Loader(const FString& SourcePath)
+	{
+		ConstructorHelpers::FObjectFinder<TypeToLoad> Finder(*SourcePath);
+		if (Finder.Succeeded())
+		{
+			_DataPtr = Finder.Object;
+		}
+#ifdef CHECK_SUCCESS_OF_LOAD_DATA
+		LoggingSystem* LogPtr = LoggingSystem::Instance(LOG_FILE_PATH);
+		std::string OutputString;
+		OutputString += TCHAR_TO_ANSI(*SourcePath);
+		OutputString+=" loading ";
+		OutputString+=BoolToCStr(Finder.Succeeded());
+		OutputString+="\n";
+		LogPtr->WriteToLog(OutputString);
+#endif
+	}
+public:
+	TypeToLoad* GetDataPtr()
+	{
+		return _DataPtr;
+	}
+private:
+	TypeToLoad* _DataPtr;
+};
+
 // Ctor functions
 void APerson::LoadAndSetSkeletalMesh(const char* PathToSkeletalMesh)
 {
@@ -50,38 +100,82 @@ void APerson::LoadAnimationAsset(const char* PathToAnimationAsset)
 void APerson::BeginPlay()
 {
 	Super::BeginPlay();
+	
 }
 
 void APerson::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
+
 }
 
 void APerson::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	// IDLE functionality
+
+	// movement callback functions:
+	PlayerInputComponent->BindAction(M_ACM_Jump,IE_Pressed,this,&APerson::ACM_Jump);
+	PlayerInputComponent->BindAxis(M_AXM_MoveForward,this,&APerson::AXM_MoveForward);
+	PlayerInputComponent->BindAxis(M_AXM_MoveRight,this,&APerson::AXM_MoveRight);
+
+	//camera controll callback fucntions:
+	PlayerInputComponent->BindAxis(M_AXM_ChangeSpringArmLength,this,&APerson::AXM_ChangeSpringArmLength);
+	PlayerInputComponent->BindAxis(M_AXM_TurnCameraX,this,&APerson::AXM_TurnCameraX);
+	PlayerInputComponent->BindAxis(M_AXM_TurnCameraY,this,&APerson::AXM_TurnCameraY);
+	
+	// animations callback funstions functionality
 	PlayerInputComponent->BindAction("IDLE_full_stamina",IE_Pressed,this,&APerson::PlayFPSIdle);
+
+}
+
+//movement callback functions:
+	//action mappings:
+void APerson::ACM_Jump()
+{
+#ifdef ENABLE_LOGGING_ON_SCREEN_ACTION_MAPPINGS
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow,"ACM Jump");
+	}
+#endif
+}
+	//axis mappings:
+void APerson::AXM_MoveForward(float AxisValue)
+{
+	
+}
+void APerson::AXM_MoveRight(float AxisValue)
+{
+	
+}
+
+//camera controll callback functions:
+	//axis mappings:
+void APerson::AXM_ChangeSpringArmLength(float AxisValue)
+{
+	
+}
+void APerson::AXM_TurnCameraX(float AxisValue)
+{
+	
+}
+void APerson::AXM_TurnCameraY(float AxisValue)
+{
 
 }
 
 //animations callback functions:
 void APerson::PlayFPSIdle()
 {
+#ifdef ENABLE_LOGGING_ON_SCREEN_ACTION_MAPPINGS
 	if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow,"playing FPS idle");
 	}
+#endif
 	GetMyMesh()->PlayAnimation(AnimationAssetPtr,true);
 }
-
-const char* BoolToCStr(bool bool_value)
-{
-	return (bool_value?"true":"false");
-}
-
 
 //getters:
 USkeletalMeshComponent* APerson::GetMyMesh() const
@@ -91,4 +185,9 @@ USkeletalMeshComponent* APerson::GetMyMesh() const
 USpringArmComponent* APerson::GetMySpringArm() const
 {
 	return SpringArmComponentPtr;
+}
+
+const char* BoolToCStr(bool bool_value)
+{
+	return (bool_value?"true":"false");
 }
--- Person.h
+++ Person.h
@@ -1,92 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Character.h"
-#include <string>
-#include "LoggingSystem.h"
+#include "GameFramework/Pawn.h"
+
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Person.generated.h"
 
-#define CHECK_SUCCESS_OF_LOAD_DATA
-
-#define PATH_SKELETAL_MESH_ARMS "/Game/FPS_Content/Assets/Characters/Arms/Mesh/Mannequin/Mesh/SK_Mannequin_Arms"
-#define PATH_ANIMATION_ASSET_FPS_IDLE "/Game/FPS_Content/Assets/Characters/Arms/Arms_Animations/Arms_idle"
-
-
-const char* BoolToCStr(bool bool_value);
-
 
 UCLASS()
-class FPS_API APerson : public ACharacter
+class FPS_API APerson : public APawn
 {
 	GENERATED_BODY()
-private:
-	//components:
-	UPROPERTY(VisibleAnywhere)
-	USkeletalMeshComponent* SkeletalMeshComponentPtr;
-	
-	UPROPERTY(VisibleAnywhere)
-	USpringArmComponent* SpringArmComponentPtr; 
 
-	UPROPERTY(VisibleAnywhere)
-	UCameraComponent* CameraComponentPtr;
-
-	//assets:
-	UPROPERTY(VisibleAnywhere)
-	UAnimationAsset* AnimationAssetPtr; 
-
-	//getters:
-protected:
-	USkeletalMeshComponent* GetMyMesh() const;
-	USpringArmComponent* GetMySpringArm() const;
 public:
+	// Sets default values for this pawn's properties
 	APerson();
+	
 	//ctor functions:
 private:
 	void LoadAndSetSkeletalMesh(const char* PathToSkeletalMesh);
 	void AssignSpringArmVaribles(const FVector& RelLocation,const FRotator& RelRotation,const double ArmLength, const double CameraLagSpeed);
 	void LoadAnimationAsset(const char* PathToAnimationAsset);
-	//begin play fucntions:
-private:
-	//animation callback functions:
-	void PlayFPSIdle();
+
+
 protected:
+	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
+	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	
+
+	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-};
-
-template<typename TypeToLoad>
-class Loader
-{
-public:
-	Loader(const FString& SourcePath)
-	{
-		ConstructorHelpers::FObjectFinder<TypeToLoad> Finder(*SourcePath);
-		if (Finder.Succeeded())
-		{
-			_DataPtr = Finder.Object;
-		}
-#ifdef CHECK_SUCCESS_OF_LOAD_DATA
-		LoggingSystem* LogPtr = LoggingSystem::Instance(LOG_FILE_PATH);
-		std::string OutputString;
-		OutputString += TCHAR_TO_ANSI(*SourcePath);
-		OutputString+=" loading ";
-		OutputString+=BoolToCStr(Finder.Succeeded());
-		OutputString+="\n";
-		LogPtr->WriteToLog(OutputString);
-#endif
-	}
-public:
-	TypeToLoad* GetDataPtr()
-	{
-		return _DataPtr;
-	}
+	
 private:
-	TypeToLoad* _DataPtr;
+	//movement callback functions:
+		//action mappings:
+	void ACM_Jump();
+		//axis mappings:
+	void AXM_MoveForward(float AxisValue);
+	void AXM_MoveRight(float AxisValue);
+	
+	//camera controll callback fucntions:
+		//axis mappings:	
+	void AXM_ChangeSpringArmLength(float AxisValue);
+	void AXM_TurnCameraX(float AxisValue);
+	void AXM_TurnCameraY(float AxisValue);
+	
+	//animation callback functions:
+	void PlayFPSIdle();
+
+	
+	
+	//getters:
+private:
+	USkeletalMeshComponent* GetMyMesh() const;
+	USpringArmComponent* GetMySpringArm() const;
+private:
+	//components:
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* SkeletalMeshComponentPtr;
+	
+	UPROPERTY(EditAnywhere)
+	USpringArmComponent* SpringArmComponentPtr; 
+
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* CameraComponentPtr;
+
+	//assets:
+	UPROPERTY(VisibleAnywhere)
+	UAnimationAsset* AnimationAssetPtr;
 };
