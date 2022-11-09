34afccf5d7bbd03489be7d1ca52c59ead6d62e17
Repnikov Pavel <prepnik@mail.ru>
2022-11-10 02:10:07 +0300

    water

--- WaterFX.cpp
+++ WaterFX.cpp
@@ -2,20 +2,61 @@
 
 
 #include "WaterFX.h"
+#include "Runtime/CoreUObject/Public/UObject/ConstructorHelpers.h"
+#include "Components/HierarchicalInstancedStaticMeshComponent.h"
+
 
 // Sets default values
 AWaterFX::AWaterFX()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	PrimaryActorTick.bStartWithTickEnabled=true;
+	MeshPtr = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("MeshPtr"));
+	SetRootComponent(MeshPtr);
+	MeshPtr->SetMobility(EComponentMobility::Static);
+	MeshPtr->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
+	MeshPtr->SetGenerateOverlapEvents(false);
+	
+	// auto MeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("/Game/MyContent/water_fx/Pyramid"));
+	// SMeshPtr = MeshAsset.Object;
+	// // MeshPtr->SetStaticMesh(MeshAsset.Object);
+	// if (MeshAsset.Object != nullptr)
+	// {
+	// 	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Mesh  found!"));
+	// }
+	//
+	// // MeshPtr = CreateDefaultSubobject<UHierarchicalInstancedStaticMeshComponent>(TEXT("HierarchicalInstancedStaticMesh"));
+	// // MeshPtr->SetupAttachment(GetRootComponent());
+	// // MeshPtr->SetCullDistances(StartCullDistance,EndCullDistance);
+	// // MeshPtr->SetStaticMesh(SMeshPtr);
+	//
+	// MeshPtr = NewObject<UHierarchicalInstancedStaticMeshComponent>(this,TEXT("name"));
+	// MeshPtr->SetupAttachment(GetRootComponent());
+	// MeshPtr->SetCullDistances(StartCullDistance,EndCullDistance);
+	// MeshPtr->SetStaticMesh(SMeshPtr);
+	// for(int k=0;k<Nz;k++)
+	// {
+	// 	for(int i=0;i<Nx;i++)
+	// 	{
+	// 		for(int j=0;j<Ny;j++)
+	// 		{
+	// 			FTransform pos_of_particle(UE::Math::TRotator<double>(0.0,0.0,0.0),
+	// 			UE::Math::TVector<double>(ax+i*hx,ay+j*hy,az+k*hz),
+	// 			UE::Math::TVector<double>(100.0,100.0,100.0));
+	// 			int index_of_particle = MeshPtr->AddInstance(pos_of_particle,false);
+	// 			
+	// 		}
+	// 	}
+	// }
+	
 }
 
 // Called when the game starts or when spawned
 void AWaterFX::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -23,5 +64,48 @@ void AWaterFX::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AWaterFX::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	Lx = bx-ax;
+	Ly=by-ay;
+	Lz=bz-az;
+	V = Lx*Ly*Lz;
+	N = Nx*Ny*Nz;
+	hx = Lx/((double)Nx);
+	hy = Lx/((double)Ny);
+	hz = Lx/((double)Nz);
+	V_kij=V/((double)N);
+	m_kij = ro*V_kij;
+
+	ReserveMemoryForI();
+	
+	for(int k=0;k<Nz;k++)
+	{
+		for(int i=0;i<Nx;i++)
+		{
+			for(int j=0;j<Ny;j++)
+			{
+				I[k][i][j] = MeshPtr->AddInstance(FTransform(FVector(ax+i*hx,ay+j*hy,az+k*hz)));
+			}
+		}
+	}
+	
+}
+
+void AWaterFX::ReserveMemoryForI()
+{
+	I.resize(Nz);
+	for (int k=0;k<Nz;k++)
+	{
+		I[k].resize(Nx);
+		for (int i=0;i<Nx;i++)
+		{
+			I[k][i].resize(Ny);
+		}
+	}
 }
 
--- WaterFX.h
+++ WaterFX.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include <vector>
 #include "WaterFX.generated.h"
 
 UCLASS()
@@ -23,4 +24,55 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+public:
+	UPROPERTY(EditAnywhere)
+	int Nx=10;
+	UPROPERTY(EditAnywhere)
+	int Ny=10;
+	UPROPERTY(EditAnywhere)
+	int Nz=10;
+	UPROPERTY(EditAnywhere)
+	int N_surf=5;
+	UPROPERTY(EditAnywhere)
+	double ax=-100.0;
+	UPROPERTY(EditAnywhere)
+	double bx=100.0;
+	UPROPERTY(EditAnywhere)
+	double ay=-100.0;
+	UPROPERTY(EditAnywhere)
+	double by=-100.0;
+	UPROPERTY(EditAnywhere)
+	double az=-100.0;
+	UPROPERTY(EditAnywhere)
+	double bz=-100.0;
+	UPROPERTY(EditAnywhere)
+	double ro=1000.0;
+
+	UPROPERTY(EditAnywhere)
+	double StartCullDistance=0.0;
+	UPROPERTY(EditAnywhere)  
+	double EndCullDistance=10000.0;
+
+public:
+	UPROPERTY(VisibleAnywhere)
+	UInstancedStaticMeshComponent* MeshPtr;
+	
+	// UStaticMesh* SMeshPtr;
+public:
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+private:
+	std::vector<std::vector<std::vector<int>>> I;
+	void ReserveMemoryForI();
+private:
+	double Lx;
+	double Ly;
+	double Lz;
+	double hx;
+	double hy;
+	double hz;
+	double V;
+	int N;
+	double V_kij;
+	double m_kij;
 };
